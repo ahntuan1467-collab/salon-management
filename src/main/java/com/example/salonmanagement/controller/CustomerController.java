88847a667df9c3fe5aad0b4cb561976e2e5c2276
ahntuan1467-collab <ahntuan1467@gmail.com>
2025-10-19 02:48:32 +0700

    lấy thoogn tin khách hàng

--- src/main/java/com/example/salonmanagement/controller/CustomerController.java
+++ src/main/java/com/example/salonmanagement/controller/CustomerController.java
@@ -1,4 +1,24 @@
 package com.example.salonmanagement.controller;
 
+import com.example.salonmanagement.model.Customer;
+import com.example.salonmanagement.service.impl.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/customers")
+@CrossOrigin(origins = "*")
 public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    // Lấy danh sách khách hàng
+    @GetMapping
+    public List<Customer> getAllCustomers() {
+        return customerService.getAllCustomers();
+    }
 }
--- src/main/java/com/example/salonmanagement/service/impl/CustomerService.java
+++ src/main/java/com/example/salonmanagement/service/impl/CustomerService.java
@@ -1,4 +1,22 @@
 package com.example.salonmanagement.service.impl;
 
+import com.example.salonmanagement.model.Customer;
+import com.example.salonmanagement.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class CustomerService {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    // Lấy danh sách khách hàng
+    public List<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
 }
