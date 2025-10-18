51e731c36a5cc7991c44292927de57d314869101
nttrang29 <thutrang.ntt215@gmail.com>
2025-10-18 21:43:32 +0700

    Màn hình hiển thị danh sách dịch vụ từ DB

--- src/main/java/com/example/salonmanagement/controller/ServiceController.java
+++ src/main/java/com/example/salonmanagement/controller/ServiceController.java
@@ -1,4 +1,28 @@
 package com.example.salonmanagement.controller;
 
+import com.example.salonmanagement.entity.ServiceEntity;
+import com.example.salonmanagement.service.impl.ServiceService;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/services")
+@CrossOrigin(origins = "*")
 public class ServiceController {
+
+    private final ServiceService serviceService;
+
+    public ServiceController(ServiceService serviceService) {
+        this.serviceService = serviceService;
+    }
+
+    @GetMapping
+    public List<ServiceEntity> getAllServices() {
+        return serviceService.getAllServices();
+    }
+
 }
