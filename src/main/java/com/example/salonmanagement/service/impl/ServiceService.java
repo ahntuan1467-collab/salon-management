e989cc611b55ee3071da0ad9127d26c343928fc0
nttrang29 <thutrang.ntt215@gmail.com>
2025-10-18 15:50:26 +0700

    Viết hàm trong Service/Repository để lấy danh sách dịch vụ từ DB

--- src/main/java/com/example/salonmanagement/service/impl/ServiceService.java
+++ src/main/java/com/example/salonmanagement/service/impl/ServiceService.java
@@ -1,4 +1,29 @@
 package com.example.salonmanagement.service.impl;
 
+import com.example.salonmanagement.entity.ServiceEntity;
+import com.example.salonmanagement.repository.ServiceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ServiceService {
+
+    @Autowired
+    private ServiceRepository repository;
+
+    public List<ServiceEntity> getAllServices() {
+        return repository.findAll();
+    }
+
 }
+
+
